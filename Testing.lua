cf7b187722024e06e5803b1a9504191e843ae816
Pogilahat <99718458+Pogilahat@users.noreply.github.com>
2022-07-27 12:24:15 +0800

    Add files via upload

---
+++ Testing.lua
@@ -0,0 +1,125 @@
+local args = {
+    [1] = "FIRSTRAIDS"
+}
+
+game:GetService("ReplicatedStorage").endpoints.client_to_server.redeem_code:InvokeServer(unpack(args))
+wait(0)
+
+--------------------------------------------------------1
+
+local args = {
+    [1] = "cxrsed"
+}
+
+game:GetService("ReplicatedStorage").endpoints.client_to_server.redeem_code:InvokeServer(unpack(args))
+wait(0)
+
+--------------------------------------------------------2
+
+local args = {
+    [1] = "subtomaokuma"
+}
+
+game:GetService("ReplicatedStorage").endpoints.client_to_server.redeem_code:InvokeServer(unpack(args))
+wait(0)
+
+--------------------------------------------------------3
+
+local args = {
+    [1] = "MARINEFORD"
+}
+
+game:GetService("ReplicatedStorage").endpoints.client_to_server.redeem_code:InvokeServer(unpack(args))
+wait(0)
+
+--------------------------------------------------------4
+
+local args = {
+    [1] = "SubToKelvingts"
+}
+
+game:GetService("ReplicatedStorage").endpoints.client_to_server.redeem_code:InvokeServer(unpack(args))
+wait(0)
+
+--------------------------------------------------------5
+
+local args = {
+    [1] = "KingLuffy"
+}
+
+game:GetService("ReplicatedStorage").endpoints.client_to_server.redeem_code:InvokeServer(unpack(args))
+wait(0)
+
+--------------------------------------------------------6
+
+local args = {
+    [1] = "SubToBlamspot"
+}
+
+game:GetService("ReplicatedStorage").endpoints.client_to_server.redeem_code:InvokeServer(unpack(args))
+wait(0)
+
+--------------------------------------------------------7
+
+local args = {
+    [1] = "FictioNTheFirst"
+}
+
+game:GetService("ReplicatedStorage").endpoints.client_to_server.redeem_code:InvokeServer(unpack(args))
+wait(0)
+
+--------------------------------------------------------8
+
+local args = {
+    [1] = "noclypso"
+}
+
+game:GetService("ReplicatedStorage").endpoints.client_to_server.redeem_code:InvokeServer(unpack(args))
+wait(0)
+
+--------------------------------------------------------9
+
+local args = {
+    [1] = "TOADBOIGAMING"
+}
+
+game:GetService("ReplicatedStorage").endpoints.client_to_server.redeem_code:InvokeServer(unpack(args))
+wait(0)
+
+--------------------------------------------------------end
+
+wait (1)
+while( true )
+do
+
+----------------------------------------1
+
+local args = {
+    [1] = "dbz_fighter",
+    [2] = "ticket"
+}
+game:GetService("ReplicatedStorage").endpoints.client_to_server.buy_random_fighter:InvokeServer(unpack(args))
+wait (1)
+
+----------------------------------------2
+
+local args = {
+    [1] = "dbz_fighter",
+    [2] = "gems10"
+}
+
+game:GetService("ReplicatedStorage").endpoints.client_to_server.buy_random_fighter:InvokeServer(unpack(args))
+wait (1)
+
+----------------------------------------3
+
+local args = {
+    [1] = "dbz_fighter",
+    [2] = "gems"
+}
+
+game:GetService("ReplicatedStorage").endpoints.client_to_server.buy_random_fighter:InvokeServer(unpack(args))
+end
+    end)
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------end
